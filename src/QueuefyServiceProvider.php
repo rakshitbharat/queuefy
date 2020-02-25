0aa915a9c06794e82c129a93e09c4e56f3adc736
Rakshit Patel <rakshitbharatproject@gmail.com>
2020-02-25 12:42:05 +0530

    - Readme changes

--- src/QueuefyServiceProvider.php
+++ src/QueuefyServiceProvider.php
@@ -11,36 +11,10 @@ class QueuefyServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        /*
-         * Optional methods to load your package assets
-         */
-        // $this->loadTranslationsFrom(__DIR__.'/../resources/lang', 'queuefy');
-        // $this->loadViewsFrom(__DIR__.'/../resources/views', 'queuefy');
-        // $this->loadMigrationsFrom(__DIR__.'/../database/migrations');
-        // $this->loadRoutesFrom(__DIR__.'/routes.php');
-
         if ($this->app->runningInConsole()) {
             $this->publishes([
                 __DIR__.'/../config/config.php' => config_path('queuefy.php'),
             ], 'config');
-
-            // Publishing the views.
-            /*$this->publishes([
-                __DIR__.'/../resources/views' => resource_path('views/vendor/queuefy'),
-            ], 'views');*/
-
-            // Publishing assets.
-            /*$this->publishes([
-                __DIR__.'/../resources/assets' => public_path('vendor/queuefy'),
-            ], 'assets');*/
-
-            // Publishing the translation files.
-            /*$this->publishes([
-                __DIR__.'/../resources/lang' => resource_path('lang/vendor/queuefy'),
-            ], 'lang');*/
-
-            // Registering package commands.
-            // $this->commands([]);
         }
     }
 
