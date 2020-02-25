c9a834a2922c26a183a73910c0c4db9533d7227d
Rakshit Patel <rakshitbharatproject@gmail.com>
2020-02-25 15:42:14 +0530

    - Fixed Code

--- config/config.php
+++ config/config.php
@@ -4,5 +4,6 @@
  * You can place your custom package configuration in here.
  */
 return [
-
+#    'QUEUE_COMMAND_FULL_PATH' => env('QUEUE_COMMAND', 'php artisan queue:work --tries=3'),
+    'QUEUE_COMMAND_AFTER_PHP_ARTISAN' => env('QUEUE_COMMAND_AFTER_PHP_ARTISAN', 'queue:work --tries=3')
 ];
--- src/ConsoleCommand.php
+++ src/ConsoleCommand.php
@@ -38,8 +38,46 @@ class ConsoleCommand extends Command
      */
     public function handle()
     {
-        if (!($PID = shell_exec("pgrep 'php /testcron.php'")))
-            echo 'Process is running with PID ' . $PID . '.';
+        $commandToExecute = "";
+        if (strtoupper(substr(PHP_OS, 0, 3)) === 'WIN') {
+            $phpPath = exec("where php");
+            $command = "tasklist | FIND ";
+        } else {
+            $phpPath = exec("which php");
+            $command = "pgrep ";
+        }
 
+
+        if (
+            empty(config('queuefy.QUEUE_COMMAND_FULL_PATH'))
+            and
+            !empty(config('queuefy.QUEUE_COMMAND_AFTER_PHP_ARTISAN'))
+        ) {
+            $commandToExecute .= $phpPath . ' ';
+            $commandToExecute .= base_path() . DIRECTORY_SEPARATOR . "artisan ";
+            $commandToExecute .= config('queuefy.QUEUE_COMMAND_AFTER_PHP_ARTISAN');
+        }
+
+        if (
+            !empty(config('queuefy.QUEUE_COMMAND_FULL_PATH'))
+            and
+            empty(config('queuefy.QUEUE_COMMAND_AFTER_PHP_ARTISAN'))
+        ) {
+            $commandToExecute .= config('queuefy.QUEUE_COMMAND_FULL_PATH');
+        }
+        $OUTPUT_FINAL = 'Nothing Done';
+        if (strtoupper(substr(PHP_OS, 0, 3)) === 'WIN') {
+            $commandToCheckProcess = $command . '"' . $commandToExecute . '"';
+        } else {
+            $commandToCheckProcess = $command . '"' . $commandToExecute . '"';
+        }
+        if (!($ID = shell_exec($commandToCheckProcess))) {
+            $ID = trim($ID);
+            $OUTPUT_FINAL = 'Process is running with PID ' . $ID . '.';
+        }
+        if(empty($ID)){
+            $OUTPUT_FINAL = shell_exec($commandToExecute);
+        }
+        echo $OUTPUT_FINAL;
     }
 }
