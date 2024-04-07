6a44ec4740ee4997d8f6f3f790fa308f9dc8fd08
GitHub <rakshitbharat@gmail.com>
2024-04-07 19:18:05 +0530

    Update QueuefyServiceProvider.php

--- src/QueuefyServiceProvider.php
+++ src/QueuefyServiceProvider.php
@@ -17,8 +17,16 @@ class QueuefyServiceProvider extends ServiceProvider
                 ConsoleCommand::class
             ]);
             $this->app->booted(function () {
-                $schedule = app(Schedule::class);
-                $schedule->command('queuefy:run')->everyMinute();
+                if (
+                    !empty(config('queuefy.STOP_QUEUE'))
+                    and
+                    !empty(config('queuefy.QUEUE_COMMAND_AFTER_PHP_ARTISAN'))
+                    and
+                    config('queuefy.STOP_QUEUE') == false
+                ) {
+                    $schedule = app(Schedule::class);
+                    $schedule->command('queuefy:run')->everyMinute();
+                }
             });
         }
     }
@@ -29,7 +37,7 @@ class QueuefyServiceProvider extends ServiceProvider
     public function register()
     {
         // Automatically apply the package configuration
-        $this->mergeConfigFrom(__DIR__.'/../config/config.php', 'queuefy');
+        $this->mergeConfigFrom(__DIR__ . '/../config/config.php', 'queuefy');
 
         // Register the main class to use with the facade
         $this->app->singleton('queuefy', function () {
