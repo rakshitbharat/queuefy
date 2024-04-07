427576cf54b743e4e37436d3c44fee4051e6ab96
GitHub <rakshitbharat@gmail.com>
2024-04-07 19:14:58 +0530

    Update config.php

--- config/config.php
+++ config/config.php
@@ -5,5 +5,6 @@
  */
 return [
 #    'QUEUE_COMMAND_FULL_PATH' => env('QUEUE_COMMAND', 'php artisan queue:work --tries=3'),
-    'QUEUE_COMMAND_AFTER_PHP_ARTISAN' => env('QUEUE_COMMAND_AFTER_PHP_ARTISAN', 'queue:work --timeout=0')
+    'QUEUE_COMMAND_AFTER_PHP_ARTISAN' => env('QUEUE_COMMAND_AFTER_PHP_ARTISAN', 'queue:work --timeout=0'),
+    'STOP_QUEUE' => env('STOP_QUEUE', false)
 ];
